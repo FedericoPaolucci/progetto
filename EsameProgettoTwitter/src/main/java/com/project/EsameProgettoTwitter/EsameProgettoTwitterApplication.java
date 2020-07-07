beb9667cae711b2b71da5d045273d57aa4f91045
pppfe <pppfe@DESKTOP-5KO57T4>
2020-07-07 16:07:29 +0200

    secondo commit mvn

---
+++ EsameProgettoTwitter/src/main/java/com/project/EsameProgettoTwitter/EsameProgettoTwitterApplication.java
@@ -0,0 +1,13 @@
+package com.project.EsameProgettoTwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsameProgettoTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsameProgettoTwitterApplication.class, args);
+	}
+
+}
---
+++ EsameProgettoTwitter/src/test/java/com/project/EsameProgettoTwitter/EsameProgettoTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.EsameProgettoTwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsameProgettoTwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
