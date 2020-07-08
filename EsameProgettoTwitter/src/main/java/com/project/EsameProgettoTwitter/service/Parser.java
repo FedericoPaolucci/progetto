afea56ad09c4cb3bd38c23ee3e11b01ed475b96b
pppfe <pppfe@DESKTOP-5KO57T4>
2020-07-08 02:38:34 +0200

    commit che completa il precedente

--- EsameProgettoTwitter/src/main/java/com/project/EsameProgettoTwitter/service/Parser.java
+++ EsameProgettoTwitter/src/main/java/com/project/EsameProgettoTwitter/service/Parser.java
@@ -9,4 +9,7 @@ package com.project.EsameProgettoTwitter.service;
  */
 public class Parser {
 
+	String url="https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/user/1.1/statuses/retweets_of_me.json";
+	String parameter="?include_entities=true";
+	
 }
